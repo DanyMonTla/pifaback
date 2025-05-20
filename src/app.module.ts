44e232d50527024327447cbee919f9fc493e432a
DanyMonTla <317172920@pcpuma.acatlan.unam.mx>
2025-05-20 09:43:52 -0600

    Proyecto NestJS inicial

---
+++ src/app.module.ts
@@ -0,0 +1,24 @@
+import { Module } from '@nestjs/common';
+import { AppController } from './app.controller';
+import { AppService } from './app.service';
+import { InidicadoresModule } from './inidicadores/inidicadores.module';
+import { IndicadoresController } from './indicadores/indicadores.controller';
+import { IndicadoresService } from './indicadores/indicadores.service';
+import { TypeOrmModule } from '@nestjs/typeorm';
+
+@Module({
+  imports: [
+    //TypeOrmModule.forRoot({
+      //type: 'mariadb', //para concectar a mariadb
+      //host: "localhost",
+      //port: 5432,
+      //username: "test",
+      //password: "test",
+      //database: "test",
+      //synchronize: true,
+     // entities: [__dirname + '/**/*.entity.ts' ],
+    InidicadoresModule],
+  controllers: [AppController, IndicadoresController],
+  providers: [AppService, IndicadoresService],
+})
+export class AppModule {}
