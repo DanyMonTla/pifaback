c2860c8ca896c91aafb9ca47fb812e86cce40d89
Yatziri-Baltazar <320335280@pcpuma.acatlan.unam.mx>
2025-06-12 23:38:08 -0600

    back bn

--- src/AreasResponsables/dto/create-AreaResponsable.dto.ts
+++ src/AreasResponsables/dto/create-AreaResponsable.dto.ts
@@ -1,27 +1,17 @@
-import {
-  IsInt,
-  IsString,
-  IsBoolean,
-  IsDateString,
-  IsOptional,
-  MaxLength,
-} from 'class-validator';
+import { IsInt, IsString, IsBoolean, IsOptional, IsDateString } from 'class-validator';
 
 export class CreateAreaDto {
   @IsInt()
   nid_area: number;
 
   @IsString()
-  @MaxLength(150)
   cunidad_responsable: string;
 
-  @IsString()
-  @MaxLength(50)
   @IsOptional()
+  @IsString()
   creporta_a?: string;
 
   @IsString()
-  @MaxLength(320)
   ccorreo_electronico_ur: string;
 
   @IsBoolean()
